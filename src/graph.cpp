df5630b1b4c3a8cff3a40dd73df1bbf2d279bf41
Mariana <73724268+golangis@users.noreply.github.com>
2023-06-04 02:55:26 +0100

    2-opt

--- src/graph.cpp
+++ src/graph.cpp
@@ -222,7 +222,7 @@ double Graph::myHeuristic(Vertex* current, Vertex* start, double dist, vector<in
     current->setVisited(true);
     pathTSP.push_back(current->getID());
 
-    if(size == pathTSP.size()){
+    if(size  == pathTSP.size()){
     //ended, put 0 and add the final distance
         pathTSP.push_back(start->getID());
         for(auto i : current->getAdj())
@@ -249,12 +249,62 @@ double Graph::myHeuristic(Vertex* current, Vertex* start, double dist, vector<in
         }
         dist = myHeuristic(minVertex, start, (dist + minCurrentDist), pathTSP, size);
     }
+    // 2-opt
+    if (pathTSP.size() == size + 1){
+        int numNodes = pathTSP.size();
+        bool improve = true;
 
+        while (improve){
+            improve = false;
+            for (int i = 0; i < numNodes - 2; ++i) {
+                for (int j = i + 2; j < numNodes; ++j) {
+                    Vertex* v1 =  findVertex(i);
+                    Vertex* v2 =  findVertex(i + 1);
+                    Vertex* v3 =  findVertex(j);
+                    Vertex* v4 =  findVertex(j + 1);
 
-    //TODO
-    // 2opt
-    // use this: Vertex* current, Vertex* start, double dist, vector<int> &pathTSP, int size
+                    double dist1, dist2, dist3, dist4;
+                    for (const auto& edge : v1->getAdj()) {
+                        if (edge->getDest() == v2) {
+                            dist1 = edge->getWeight();
+                            break;
+                        }
+                    }
+                    for (const auto& edge : v3->getAdj()) {
+                        if (edge->getDest() == v4) {
+                            dist2 = edge->getWeight();
+                            break;
+                        }
+                    }
+                    for (const auto& edge : v1->getAdj()) {
+                        if (edge->getDest() == v3) {
+                            dist3 = edge->getWeight();
+                            break;
+                        }
+                    }
+                    for (const auto& edge : v2->getAdj()) {
+                        if (edge->getDest() == v4) {
+                            dist4 =  edge->getWeight();
+                            break;
+                        }
+                    }
 
+                    // Fazer a troca
+                    double newDist = dist - dist1 - dist2;
+                    newDist += dist3 + dist4;
+
+                    // Se o novo caminho for mais curto, aceitar a troca
+                    if (newDist < dist) {
+                        improve = true;
+                        dist = newDist;
+                        std::reverse(pathTSP.begin() + i + 1, pathTSP.begin() + j + 1);
+                    }
+                }
+            }
+
+            }
+
+    }
 
     return dist;
 }
